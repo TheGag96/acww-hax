55a7ae1e7d811e664ad0bfa9852ef21be5d151fb
TheGag96 <thegag96@gmail.com>
2023-03-19 01:01:52 -0500

    Add Skin Tan Never Changes mod for USA v1.0 and PAL
    This mod makes it so your skin tan never gets lighter or darker -
    intended to be used in tandom with save editors to permanently change
    your skin color.
    
    In the future, I'd like to implement this in a more robust way, with a
    whole bit of custom dialog with Kapp'n at the beginning to choose your
    skin color, but that's gonna be some work lol.

---
+++ mods/Skin Tan Never Changes (PAL)/code/SetSkinTanNop.s
@@ -0,0 +1,4 @@
+.thumb
+
+SetSkinTanNop: @ overwrite 0x02098b1c
+  bx lr  @ nop the whole function
---
+++ mods/Skin Tan Never Changes/code/SetSkinTanNop.s
@@ -0,0 +1,4 @@
+.thumb
+
+SetSkinTanNop: @ overwrite 0x0209852c
+  bx lr  @ nop the whole function
